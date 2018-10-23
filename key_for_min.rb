4a69019a0def3bac4669b74ab3c924d192d5be97
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-23 16:32:18 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,16 +4,11 @@
 def key_for_min_value(name_hash)
   lowest_value = nil
   lowest_key = nil
-  if name_hash.length > 0
-    name_hash.each do |key, value|
-      if lowest_value == nil || value < i
-      smallest = name
-      i = value
-      end
+  name_hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
     end
-      smallest
-  # else
-  #   nil
   end
 end
 
